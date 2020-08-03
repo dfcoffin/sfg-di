a44869788f2161dc24e756f7358be9e3aef53298
Donald F. Coffin <donald.coffin@reminetworks.com>
2020-08-03 14:32:52 -0400

    adding init spring project

---
+++ src/test/java/com/reminetworks/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reminetworks.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
